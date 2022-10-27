f2a96a30f9559af42ef83dbe10703e7f2b2c5762
jamschutz <52084893+jamschutz@users.noreply.github.com>
2022-10-27 17:37:11 -0400

    Joey (#3)
    * updated prefab loader to destroy camera
    
    * can show dialog, skip to the end, etc
    
    * dialog controller works
    
    * updated prefab
    
    * dialog box done
    
    * can talk to the captain
    
    * captain can follow you!
    
    * changed hao's scene

---
+++ Assets/Scripts/AI/Controllers/AIController.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace AI.Controller
+{
+    public class AIController : MonoBehaviour
+    {
+        public enum State { Idle, FollowPlayer }
+
+        [Header("Movement")]
+        public float moveSpeed;
+        public float followDistance;
+
+        protected State state;
+
+        protected Transform currentTarget;
+        protected Transform player;
+
+
+        protected void Init()
+        {
+            player = GameObject.FindGameObjectWithTag("Player").transform;
+            currentTarget = player;
+        }
+
+
+
+        protected void MoveTowardsTarget()
+        {
+            // if we're too close, don't move
+            if(Vector2.Distance(currentTarget.position, transform.position) <= followDistance) {
+                Debug.Log("too close!!!");
+                return;
+            }
+
+            // otherwise, move!
+            var moveDirection = (currentTarget.position - transform.position).normalized;
+            transform.Translate(moveDirection * moveSpeed * Time.deltaTime, Space.World);
+        }
+    }
+}
+
